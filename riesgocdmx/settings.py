7bfa0ac57bf66bc7f390154c6146a23b6ba704f1
Ivan252512 <ivanpineda@ciencias.unam.mx>
2019-03-08 11:56:12 -0600

    heroku

--- riesgocdmx/settings.py
+++ riesgocdmx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zd#i)c)7$5b@j%ov7(7=vmc%7gvouhn_i8-2xx@l)3t9_363%u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.71','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
