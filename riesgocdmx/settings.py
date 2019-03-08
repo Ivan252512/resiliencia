411372236f7b8dd3704ffe2b8bf00af1e3836971
Ivan252512 <ivanpineda@ciencias.unam.mx>
2019-03-08 11:21:28 -0600

    heroku

--- riesgocdmx/settings.py
+++ riesgocdmx/settings.py
@@ -145,3 +145,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env) 
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
